0206507ac96073a39ad705afc95add0f8eb0367c
Ben Goff <bengoff@gmail.com>
2022-03-01 14:25:48 -0500

    add tesseract connector

--- db_connections.py
+++ db_connections.py
@@ -143,5 +143,31 @@ class sqlite_db(object):
     def close_db(self):
         self.connection.close()
 
+class tesseract_connection(object):
+    def __init__(self):
+        with open("settings.conf", "r") as f:
+            settings = json.load(f)
+            server = settings["tesseract_server"]
+            port = settings["tesseract_port"]
+            username = settings["tesseract_user"]
+            password = settings["tesseract_password"]
+        self.conn = trino.dbapi.connect(
+            host=server,
+            port=port,
+            user=username,
+            auth=trino.auth.BasicAuthentication(username,  password),
+            http_scheme="https")
+        self.cur = self.conn.cursor()
+
+    def execute(self, query, dict=False):
+        data = self.cur.execute(query) #.fetchall()
+        if dict:
+            if len(data[0]) == 2:
+                d = defaultdict(list)
+                for r in data:
+                    d[r[0]].append(r[1])
+                return d
+        return [list(i) for i in data]
+
 if __name__ == "__main__":
     pass
