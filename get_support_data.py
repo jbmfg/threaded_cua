fea660d8f6c62ace1d8bc8c0992a519f1853db7f
Ben Goff <bengoff@gmail.com>
2022-05-20 12:29:09 -0400

    move to docs.vmware.com for EOL data

--- get_support_data.py
+++ get_support_data.py
@@ -1,66 +1,53 @@
 import datetime
+import dateparser
+import requests
+import re
 from community import community_connection
 from bs4 import BeautifulSoup
 from db_connections import sqlite_db
 from collections import defaultdict
 
 def get_support_data(db):
-    session_id = ""
-    community = community_connection(session_id=session_id)
-    # EOL Data https://community.carbonblack.com/t5/Documentation-Downloads/Carbon-Black-Product-Release-Lifecycle-Status/ta-p/39757
-
-    query = "SELECT body FROM messages WHERE id = '39757'"
-    body = community.get_data(query)["data"]["items"][0]["body"]
-    soup = BeautifulSoup(body, features="html.parser")
-
-    tables = {}
-    ps = soup.find_all("p", recursive=False)
-    for x, i in enumerate(ps):
-        if i.get_text() == "Carbon Black Cloud":
-            tables["psc_win"] = i.findNext("table")
-            tables["psc_mac"] = tables["psc_win"].findNext("table")
-            tables["psc_lin"] = tables["psc_mac"].findNext("table")
-    table_rows = defaultdict(list)
-    for product in tables:
-        trs = tables[product].find_all("tr")
-        for tr in trs:
-            table_rows[product].append([td.get_text().replace("\n", "").replace("\u00a0", "").replace(" (latest)", "") for td in tr.find_all("td")])
-
-    rows = []
-    for os in table_rows:
-        for d in table_rows[os][1:]:
-            rows.append([os[-3:].upper()] + d)
-
-    for x, r in enumerate(rows):
-        for d in r[2:]:
-            if d.strip() == "TBD": continue
-            if not d: continue
-            try:
-                rows[x][r.index(d)] = datetime.datetime.strptime(d.strip().replace("Sept", "Sep"), "%b-%y")
-            except ValueError:
-                try:
-                    rows[x][r.index(d)] = datetime.datetime.strptime(d, "%B-%y")
-                except ValueError:
-                    try:
-                        rows[x][r.index(d)] = datetime.datetime.strptime(d, "%B-%Y")
-                    except ValueError:
-                        try:
-                            rows[x][r.index(d)] = datetime.datetime.strptime(d.replace("Sept", "Sep"), "%b %Y")
-                        except ValueError:
-                            try:
-                                rows[x][r.index(d)] = datetime.datetime.strptime(d, "%B %Y")
-                            except ValueError:
-                                try:
-                                    rows[x][r.index(d)] = datetime.datetime.strptime(d, "%b \'%y")
-                                except ValueError:
-                                    try:
-                                        rows[x][r.index(d)] = datetime.datetime.strptime(d, "%B \'%y")
-                                    except:
-                                        raise
+    urls = [
+        ["psc_win", "/cbc-oer-win-sensor-on-desktop/GUID-6197EE6C-3CCD-4F4B-9DCD-110804251ACC.html"],
+        ["psc_lin", "/cbc-oer-linux-sensor/GUID-8A478FFF-656E-46EC-8904-50BD08DA542A.html"],
+        ["psc_mac", "/cbc-oer-macos-sensor/GUID-D4E1F76A-DDBA-43E2-A651-DFB7ACBAF621.html"]
+        ]
+    levels = []
+    for os, url in urls:
+        url = "https://docs.vmware.com/en/VMware-Carbon-Black-Cloud/services" + url
+        data = requests.get(url).content
+        soup = BeautifulSoup(data, features="html.parser")
+        table_bodies = soup.find_all("tbody")
+        for i in table_bodies:
+            rows = i.find_all("tr")
+            row_data = []
+            for row in rows:
+                entries = row.find_all("td")
+                items = [entry.get_text() for entry in entries]
+                if any(items):
+                    levels += [[os] + items]
+    for x, row in enumerate(levels):
+        for xx, ri in enumerate(row):
+            # First two items are os and version
+            if xx < 2: continue
+            # Parse the date fields
+            matches = re.match(r'([a-zA-Z]+).+?([\d]+)', ri.strip())
+            mon, year = matches.groups()[0], matches.groups()[1]
+            if len(year) == 2:
+                year = f"20{year}"
+            mon_year = f"{mon} {year}"
+            mon_year = f"{year}-{mon}-01"
+            levels[x][xx] = dateparser.parse(mon_year).date().isoformat()
+    fields = ["Product", "Release", "Enter Standard", "Enter Extended", "Enter End of Life"]
 
     # Calculate where the support is right now
     now = datetime.datetime.now()
-    for x, r in enumerate(rows):
+    date_format = "%Y-%m-%d"
+    for x, r in enumerate(levels):
+        levels[x][2] = datetime.datetime.strptime(r[2], date_format)
+        levels[x][3] = datetime.datetime.strptime(r[3], date_format)
+        levels[x][4] = datetime.datetime.strptime(r[4], date_format)
         # Just the dates
         sdates = r[2:]
         if not isinstance(sdates[2], str) and now > sdates[2]:
@@ -69,10 +56,10 @@ def get_support_data(db):
             lvl = "EX"
         elif not isinstance(sdates[0], str) and now > sdates[0]:
             lvl = "ST"
-        rows[x].append(lvl)
+        levels[x].append(lvl)
 
     fields = ["os", "version", "standard", "extended", "eol", "current_level"]
-    db.insert("version_support", fields, rows, pk=False, del_table=True)
+    db.insert("version_support", fields, levels, pk=False, del_table=True)
 
 if __name__ == "__main__":
     db = sqlite_db("cua.db")
